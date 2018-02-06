3c012efaa8cae845c3816fe5553a2cea865606d2
blehalklos <klosowskiadan@gmail.com>
2018-02-06 11:41:22 -0300

    clase 2

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
+   var importe
+   var iva
+   var resultado
+   importe= prompt ("ingrese el importe")
+   importe= parseInt(importe)
+   iva= importe*0.21
+   resultado= importe+iva
+   document.getElementById("importeFinal").value=resultado
+
   
 }
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,15 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var largo
+    var ancho
+    var perimetro
+    largo=document.getElementById("largo").value;
+    ancho=document.getElementById("ancho").value;
+    largo= parseInt(largo)
+    ancho= parseInt(ancho)
+    perimetro=largo*ancho
+    alert ("usted necesita" + perimetro*6 + "metros de cable")
 	
 }
 
